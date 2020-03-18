e7b4ac7ca4f5829e73a0285ee48a9f8a5f3ed7ed
Vladimir <example@gmail.com>
2020-03-18 15:19:17 +0300

    add objects and methods

---
+++ test.js
@@ -0,0 +1,20 @@
+let appData = {
+    income: [],
+    chooseIncome: function () {
+
+        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        } else {
+            appData.income = items.split(", ");
+            appData.income.push(prompt("Может что-то еще?"));
+            appData.income.sort();
+        }
+
+        appData.income.forEach (function (itemmassive, i) {
+            alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive);
+        });
+
+    }
+};
